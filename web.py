50e405aebcde299bcd4e380ae26ae6e84ed7ea37
txcalon-z <txc.alon@gmail.com>
2023-03-01 23:24:00 +0800

    init

---
+++ web.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+#创建了网址/show/info 和函数index的关系
+#以后用户在浏览器访问/show/info，网站自动执行index
+@app.route("/show/info")
+def index():
+    return "中国电信"
+
+if __name__ == "__main__":
+    app.run()
